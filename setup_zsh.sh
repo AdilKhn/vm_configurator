6e24b66bdc6e0a26962d92ade0c1b489fbf8c822
Adil Khan <adil.khan@outlook.com>
2025-07-21 19:26:24 -0400

    wip

--- setup_zsh.sh
+++ setup_zsh.sh
@@ -5,5 +5,5 @@ sudo apt install zsh
 sudo chsh -s $(which zsh) $USER
 
 #setup startshp and zim
-curl -sS https://starship.rs/install.sh | starship
 curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
+curl -sS https://starship.rs/install.sh | sh
